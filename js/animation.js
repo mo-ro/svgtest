863aaffc1b249382206d532c0ef4c65d8d115a56
mo-ro <gainmoro@gmail.com>
2017-08-18 01:47:04 +0900

    finish

--- js/animation.js
+++ js/animation.js
@@ -1,14 +1,1 @@
-//var mySVG = $('#element').drawsvg({
-//    
-//});
-//mySVG.drawsvg('animate');
-
-var $doc = $(document),
-$win = $(window),
-$svg = $('#element').drawsvg(),
-max = $doc.height() - $win.height();
-  
-$win.on('scroll', function() {
-var p = $win.scrollTop() / max;
-$svg.drawsvg('progress', p);
-});
+skrollr.init();
